ec1b198f7997e6a133221073cde7bc4b56d9056c
Abdisamad <abdisamad@web-epoch.com>
2017-05-24 13:58:13 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <link rel="manifest" href="manifest.json">
+  <link rel="manifest" href="{{ url('/manifest.json') }}">
   <meta name="theme-color" content="#f27b00">
   <title>AngularTaskApp</title>
   <base href="https://angular.shaybix.com">
@@ -10,8 +10,13 @@
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link href="styles.ef9a26d4778a811fba16.bundle.css" rel="stylesheet"/></head>
+<link href="{{ url('/styles.ef9a26d4778a811fba16.bundle.css') }}" rel="stylesheet"/></head>
 <body>
   <app-root>Loading...</app-root>
-<script type="text/javascript" src="inline.308d4459d130af8f7c38.bundle.js"></script><script type="text/javascript" src="polyfills.d6ace064705e27ed776a.bundle.js"></script><script type="text/javascript" src="sw-register.b73048fe3d9f8a1e7ae5.bundle.js"></script><script type="text/javascript" src="vendor.3238ecd50ec3202a827c.bundle.js"></script><script type="text/javascript" src="main.b1d3db0fc01794fffba0.bundle.js"></script></body>
+<script type="text/javascript" src="{{ url('inline.308d4459d130af8f7c38.bundle.js') }}"></script>
+  <script type="text/javascript" src="{{url('polyfills.d6ace064705e27ed776a.bundle.js')}}"></script>
+  <script type="text/javascript" src="{{ url('sw-register.b73048fe3d9f8a1e7ae5.bundle.js') }}"></script>
+  <script type="text/javascript" src="{{ url('vendor.3238ecd50ec3202a827c.bundle.js') }}"></script>
+  <script type="text/javascript" src="{{ url('main.b1d3db0fc01794fffba0.bundle.js') }}"></script>
+</body>
 </html>
